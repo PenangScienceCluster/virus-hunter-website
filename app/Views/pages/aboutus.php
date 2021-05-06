8f1ae6a9721636e072a7f63888a9f4da42160b23
andyloong <andyloong84@gmail.com>
2021-05-06 10:26:11 +0800

    udpates

--- app/Views/pages/aboutus.php
+++ app/Views/pages/aboutus.php
@@ -16,9 +16,9 @@
 <div class="container h-100">
   <div class="row align-items-center h-100">
     <div class="col-lg-12 col-10 mx-auto bg animated fadeIn">
-      <div class="position-absolute gina w-25 animated fadeInLeft delay-3s"><img src="/img/characters/gina-1.png"
+      <div class="position-absolute gina w-25 animated fadeInLeft delay-2s"><img src="/img/characters/gina-1.png"
           class="img-fluid" /></div>
-      <div class="position-absolute ilyas w-20 animated fadeInRight delay-3s"><img src="/img/characters/ilyas-1.png"
+      <div class="position-absolute ilyas w-20 animated fadeInRight delay-2s"><img src="/img/characters/ilyas-1.png"
           class="img-fluid" /></div>
       <div class="row align-items-center mx-auto h-100">
         <div class="col-10 mx-auto text-center ">
@@ -29,7 +29,7 @@
             <?= lang('App.aboutus.desc') ?>
           </h4>
 
-          <div class="logo-base w-60 mx-auto  animated fadeIn delay-2s">
+          <div class="logo-base w-60 mx-auto  animated fadeIn delay-1s">
             <div class="row">
               <div class="col-6">
                 <div class="text-left">
