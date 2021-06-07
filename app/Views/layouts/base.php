f181d8c2475d1479a6baaafb228317d6731b9cb7
andyloong <andyloong84@gmail.com>
2021-06-07 17:48:13 +0800

    updates

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -10,7 +10,8 @@
   <!-- prefix -->
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no" />
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website">
