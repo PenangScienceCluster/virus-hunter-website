70ef1f1187bdda690ff2f19b2b3d7276e0d65f99
andyloong <andyloong84@gmail.com>
2021-06-08 02:01:31 +0800

    updates

--- app/Views/layouts/pop_content.php
+++ app/Views/layouts/pop_content.php
@@ -9,7 +9,7 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <div class="popup-content my-4">
+        <div class="popup-content my-2">
           <div class="w-60 mx-auto"><img src="<?= $img ?>" class="img-fluid" />
           </div>
           <div class="popup-text mt-4">
